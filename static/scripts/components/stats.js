9ac290a6124b3b0c8734f42ace25a16190d04f72
catalinmiron <mironcatalin@gmail.com>
2015-04-18 23:15:20 +0300

    Add custom stats #1
    Create barChart using css ( flexbox FTW ) ^^

--- static/scripts/components/stats.js
+++ static/scripts/components/stats.js
@@ -20,25 +20,62 @@ let Stats = React.createClass({
   },
 
   componentWillMount() {
+    /*
+      Endpoint response for timeseries
+      {
+        1428537600: 12,
+        1428624000: 0,
+        1428710400: 32
+      }
+    */
     fetch(window.TIMESERIES + "?since=1428613681")
     .then((res) => {
         return res.json();
     }).then((json) => {
-      var barData = [];
-      _.forEach(json, (data) => {
-        barData.push(data);
+      let barData = [];
+
+      Object.keys(json).map((k) => {
+        barData.push({
+          timeframe: parseInt(k),
+          value: json[k]
+        });
       });
+
       this.setState({
-        data: barData
+        data: barData,
+        maxValue: _.max(json)
       });
     });
   },
 
   render() {
-    return <div className="stats stressed-content">
-      {!_.isEmpty(this.state.data) ? this.state.data
-                                   : null}
+    return <div className="stats">
+      {!_.isEmpty(this.state.data) ? this.renderBarColumn() : null}
     </div>;
+  },
+
+  renderBarColumn() {
+    return _.map(this.state.data, (data) => {
+      let barHeight = Math.round(data.value / this.state.maxValue * 100) + "%";
+      let style = {
+        height: data.value === 0 ? "100%" : barHeight
+      };
+      let barClasses = React.addons.classSet({
+        "bar": true,
+        "bar-with-no-value": data.value === 0
+      });
+
+      return <div className={barClasses}>
+        <div className="bar-column">
+          <div className="value" style={style}>
+            <span>{data.value}</span>
+          </div>
+        </div>
+        <div className="date">
+          {moment(data.timeframe).format(this.constructor.DATE_FORMAT)}
+        </div>
+      </div>;
+    });
   }
 });
 
